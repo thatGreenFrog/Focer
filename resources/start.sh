4f4cf8d8c257deb0e195b697d34ecb047dc397e5
Martins <trubinsh@gmail.com>
2020-06-02 22:36:30 +0300

    code refactoring

--- resources/start.sh
+++ resources/start.sh
@@ -1,1 +1,1 @@
-/home/crawler/soft/soft/bin/storm jar focer-0.1.jar lv.greenfrog.crawler.FocerCrawlTopology -conf config.yaml
+/home/crawler/soft/storm/bin/storm jar focer-0.1.jar lv.greenfrog.crawler.FocerCrawlTopology -conf config.yaml
