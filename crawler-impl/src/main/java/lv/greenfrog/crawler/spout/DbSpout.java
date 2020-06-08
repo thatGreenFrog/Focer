c07c7cdc0f8f12c8a57b4e5e9320346db01c24f7
Martins <trubinsh@gmail.com>
2020-06-08 23:36:57 +0300

    removed redundancy

--- crawler-impl/src/main/java/lv/greenfrog/crawler/spout/DbSpout.java
+++ crawler-impl/src/main/java/lv/greenfrog/crawler/spout/DbSpout.java
@@ -3,19 +3,17 @@ package lv.greenfrog.crawler.spout;
 import com.digitalpebble.stormcrawler.Metadata;
 import com.digitalpebble.stormcrawler.persistence.AbstractQueryingSpout;
 import com.digitalpebble.stormcrawler.util.ConfUtils;
-import com.digitalpebble.stormcrawler.util.StringTabScheme;
 import lv.greenfrog.crawler.SessionManager;
 import lv.greenfrog.crawler.persistence.LinksMapper;
 import lv.greenfrog.crawler.persistence.entity.Links;
 import org.apache.ibatis.session.SqlSession;
-import org.apache.storm.spout.Scheme;
 import org.apache.storm.spout.SpoutOutputCollector;
 import org.apache.storm.task.TopologyContext;
 import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.tuple.Fields;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.nio.ByteBuffer;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
@@ -24,8 +22,6 @@ import java.util.Map;
 public class DbSpout extends AbstractQueryingSpout {
 
     private static final Logger log = LoggerFactory.getLogger(DbSpout.class);
-
-    private static final Scheme SCHEME = new StringTabScheme();
 
     private String resourceFolder;
 
@@ -61,8 +57,7 @@ public class DbSpout extends AbstractQueryingSpout {
         SqlSession session = SessionManager.getSession(resourceFolder);
         Links link = session.getMapper(LinksMapper.class).getByScore();
         if(link != null) {
-            List linkData = SCHEME.deserialize(ByteBuffer.wrap((link.getLink()).getBytes()));
-            buffer.add((String)linkData.get(0), (Metadata)linkData.get(1));
+            buffer.add(link.getLink(), new Metadata());
             session.getMapper(LinksMapper.class).updateVisited(link);
             session.commit();
             session.close();
@@ -71,6 +66,6 @@ public class DbSpout extends AbstractQueryingSpout {
 
     @Override
     public void declareOutputFields(OutputFieldsDeclarer declarer) {
-        declarer.declare(SCHEME.getOutputFields());
+        declarer.declare(new Fields("url", "metadata"));
     }
 }
